41fe5ded595576d4b4f2ce06417d8c3944f8b710
ozgurozbekuk <ozgurozbek.uk@gmail.com>
2024-01-19 21:06:04 +0200

    updated

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import "./admin.css"
 
 function Admin() {
   return (
-    <div>
+    <div className='admin'>
       <h1>Lorem ipsum dolor sit amet.</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae itaque, sit delectus vel tempora voluptatem.</p>
     </div>
