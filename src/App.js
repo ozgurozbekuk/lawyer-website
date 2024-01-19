90086d225559d8d4cb965cefcc464e4e589401d0
ozgurozbekuk <ozgurozbek.uk@gmail.com>
2024-01-19 21:34:59 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Navbar from "./components/header/navbar/Navbar";
+
 import About from "./components/about/About";
 import Services from "./components/services/Services";
-import Icon from "./components/header/Icons/Icon";
 import Team from "./components/team/Team";
 import Contact from "./components/contact/Contact";
 import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import Blog from "./pages/blog/Blog";
 import Admin from "./pages/admin/Admin";
 import Home from "./components/home/Home";
+import Header from "./components/header/Header";
 
 
 function App() {
@@ -15,8 +15,7 @@ function App() {
     <div className="App">
     
     <Router>
-        <Icon />
-        <Navbar />
+        <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Icon from './Icons/Icon'
+import Navbar from './navbar/Navbar'
+
+function Header() {
+  return (
+    <div>
+      <Icon/>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Header
